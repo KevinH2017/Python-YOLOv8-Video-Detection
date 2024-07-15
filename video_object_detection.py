1414009a32075e0373d0ab7d2bf0be0a38b71b58
KevinH <khui98@gmail.com>
2024-07-15 13:01:24 -0400

    Initial Upload
    Uploaded video_object_detection.py and video dog_video1.mp4 with the output .mp4 file.

---
+++ video_object_detection.py
@@ -0,0 +1,39 @@
+from ultralytics import YOLO
+import cv2
+
+# Path to video file
+video_path = "./videos/dog_video1.mp4"
+video_path_out = '{}_out.mp4'.format(video_path)
+
+cap = cv2.VideoCapture(video_path)
+ret, frame = cap.read()
+H, W, _ = frame.shape
+out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'mp4v'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
+
+# Path to .pt file (under runs or models)
+model_path = "./models/DogDetector.pt"
+
+# Load a model
+model = YOLO(model_path)  # load a custom model
+
+# Threshold for minimum prediction value
+threshold = 0.1
+# Creates video with bounding boxes and saves to mp4 file
+while ret:
+
+    results = model(frame)[0]
+
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = result
+
+        if score > threshold:
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+            cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+
+    out.write(frame)
+    ret, frame = cap.read()
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
